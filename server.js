58b8a7d0742ae2b5f055df789d8fd8f60b31ce1b
Jesus Gutierrez <jesusgut24@gmail.com>
2015-11-26 16:10:18 -0300

    Usar template strings (ES6)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 
 const server = http.createServer()
@@ -11,10 +12,17 @@ server.on('request', onRequest)
 server.on('listening', onListening)
 
 function onRequest(request, response){
-	let file = fs.readFileSync('public/index.html')
-	response.end(file)
+	let index = path.join(__dirname, 'public', 'index.html')
+	response.setHeader('Content-Type', 'text/html')
+	let rs = fs.createReadStream(index)
+	rs.pipe(response)
+
+	rs.on('error', function(error){
+		response.setHeader('Content-Type', 'text/html')
+		response.end(error.message)
+	})
 }
 
 function onListening(){
-	console.log('El server esta escuchando el puerto '+port)
+	console.log(`El server esta escuchando el puerto ${port}`)
 }
