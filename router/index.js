15222bcd502bb9097b17b0d01b103f047dd66173
Jesus Gutierrez <jesusgut24@gmail.com>
2015-11-29 14:37:52 -0430

    Agregar course para manejo de rutas

--- router/index.js
+++ router/index.js
@@ -1,17 +1,23 @@
 const path = require('path')
+const course = require('course')
 const st = require('st')
 
+const router = course()
 const mount = st({
 	path: path.join(__dirname, '..', 'public'),
-	index: 'index.html'
+	index: 'index.html',
+	passThrough: true
 })
 
 function onRequest(request, response){
 	mount(request, response, function(error){
-		if(error) return response.end(error.message)
+		if(error) return fail(error, response)
 
-		response.statusCode = 404
-		response.end(`Not found ${request.url}`)
+		router(request, response, function(error){
+			if(error) return fail(error, response)
+			response.statusCode = 404
+			response.end(`Not found ${request.url}`)
+		})
 	})
 }
 
