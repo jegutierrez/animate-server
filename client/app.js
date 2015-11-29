f98afd94f5a69d6a65a811ffeb05f69065709671
Jesus Gutierrez <jesusgut24@gmail.com>
2015-11-29 15:04:56 -0430

    Agregar ruta /process y enviar imagenes para procesamiento, Estas imagenes seran capturadas utilizando el modulo  en el browser y las enviaremos con una peticion ajax utilizando el modulo

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 const Webrtc2images = require('webrtc2images')
+const xhr = require('xhr')
 
 const rtc = new Webrtc2images({
 	width: 200,
@@ -19,6 +20,19 @@ record.addEventListener('click', function(e){
 	e.preventDefault()
 
 	rtc.recordVideo(function(err, frames){
-		console.log(frames)
+		if(err) return logError(err)
+		xhr({
+			url: '/process',
+			method: 'post',
+			headers: {'Content-Type':'application/json'},
+			body: JSON.stringify({images:frames})
+		}, function(err, res, body){
+			if(err) return logError(err)
+			console.log(JSON.parse(body))
+		})
 	})
 }, false)
+
+function logError (err) {
+  console.error(err)
+}
--- router/index.js
+++ router/index.js
@@ -1,12 +1,24 @@
 const path = require('path')
 const course = require('course')
 const st = require('st')
+const jsonBody = require('body/json')
 
 const router = course()
 const mount = st({
 	path: path.join(__dirname, '..', 'public'),
 	index: 'index.html',
 	passThrough: true
+})
+
+router.post('/process', function(req, res){
+	jsonBody(req, res, {limit: 5*1024*1024}, function(err, body){
+		if(err) return fail(err, res)
+
+		console.log(body)
+
+		res.setHeader('Content-Type', 'application/json')
+		res.end(JSON.stringify({ok:'true'}))
+	})
 })
 
 function onRequest(request, response){
@@ -19,6 +31,12 @@ function onRequest(request, response){
 			response.end(`Not found ${request.url}`)
 		})
 	})
+}
+
+function fail(err, res){
+	res.statusCode = 500
+	res.setHeader('Content-Type', 'text/plain')
+	res.end(err.message)
 }
 
 module.exports = onRequest
