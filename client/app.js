1de0618537ca3aea140cd6f7c19ab754f75beb70
Jesus Gutierrez <jesusgut24@gmail.com>
2015-12-03 11:17:04 -0300

    Creamos un flujo de chat basico sando el modulo  para requerir templates de , tambien el modulo  que me convierte html a objetos del  y asi poder utilizar JavaScript nativo en el browser

--- client/app.js
+++ client/app.js
@@ -1,5 +1,7 @@
 const Webrtc2images = require('webrtc2images')
 const xhr = require('xhr')
+const domify = require('domify')
+const messageTpl = require('./templates/message.hbs')
 
 const rtc = new Webrtc2images({
 	width: 200,
@@ -14,10 +16,18 @@ rtc.startVideo(function (err) {
    if (err) return logError(err)
 })
 
-const record = document.querySelector('#record')
+const messages = document.querySelector('#messages')
+const form = document.querySelector('form')
 
-record.addEventListener('click', function(e){
+form.addEventListener('submit', function(e){
 	e.preventDefault()
+	record()
+}, false)
+
+function record(){
+	const input = document.querySelector('input[name="message"]')
+	const message = input.value
+	input.value = ''
 
 	rtc.recordVideo(function(err, frames){
 		if(err) return logError(err)
@@ -31,14 +41,16 @@ record.addEventListener('click', function(e){
 	        body = JSON.parse(body)
 	  
 	        if (body.video) {
-	          const video = document.querySelector('#video')
-	          video.src = body.video
-	          video.loop = true
-	          video.play()
+	          addMessage({message: message, video: body.video})
 	        }
 		})
 	})
-}, false)
+}
+
+function addMessage(message){
+	const m = messageTpl(message)
+	messages.appendChild(domify(m))
+}
 
 function logError (err) {
   console.error(err)
